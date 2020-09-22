29373366c57eab3c9bf96a4323207c9c368c8328
WLQ <593768341@qq.com>
2020-09-22 20:14:14 +0800

    Constructor 字段初始化

--- CoreJava/src/java/CH04/Constructor.java
+++ CoreJava/src/java/CH04/Constructor.java
@@ -1,5 +1,7 @@
 package CH04;
 
+import java.util.Random;
+
 /**
  * 重载构造器
  * 用this()调用另一个构造器
@@ -7,6 +9,97 @@ package CH04;
  * 对象初始化块
  * 静态初始化块
  * 实例字段初始化
+ * @author QingHappy
  */
 public class Constructor {
+    public static void main(String[] args) {
+        var school = new Student[3];
+        school[0] = new Student("Tsing", 100);
+        school[1] = new Student(95);
+        school[2] = new Student();
+
+        for (Student s : school) {
+            System.out.println(s.toString());
+        }
+
+    }
+
+}
+
+class Student {
+    private static int nextId;
+    private int id;
+    private int grage;
+    /**
+     * 初始化数据字段方法一
+     * @param name 显示字段初始化为小王
+     */
+    private String name = "小王";
+
+
+    /**
+     * 初始化字段方法二
+     * 使用初始化块机制，当构造这个类的对象时，这些初始化块就会被执行！
+     *   1. 对象初始化块
+     *   2. 静态初始化块
+     */
+    static
+    {
+        var generator = new Random();
+        nextId = generator.nextInt(10000);
+    }
+
+    {
+        id = nextId;
+        nextId++;
+    }
+
+    /**
+     * 初始化数据字段方法三
+     * 使用有参构造器，在构造器中赋值
+     */
+    public Student(String n, int g) {
+        name = n;
+        grage = g;
+    }
+
+    /**
+     *
+     * 使用this()另一个构造器
+     */
+    public Student(int g) {
+        this("Student#" + nextId, g);
+    }
+
+    /**
+     * 初始化字段默认方法
+     * 无参构造，此时默认：
+     *      1.数值为0
+     *      2.布尔值为false
+     *      3.对象引用为null，String也为null
+     */
+    public Student() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getGrage() {
+        return grage;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", grage=" + grage +
+                '}';
+    }
 }
