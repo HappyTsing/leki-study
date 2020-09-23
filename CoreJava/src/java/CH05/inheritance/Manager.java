8a89a13b9b79a2a5e862f5835bb86fca3997d4a9
WLQ <593768341@qq.com>
2020-09-23 13:04:58 +0800

    inheritance 继承之多态和动态绑定

---
+++ CoreJava/src/java/CH05/inheritance/Manager.java
@@ -0,0 +1,38 @@
+package CH05.inheritance;
+
+/**
+ * 继承的具体实现
+ * super关键字的两种用法
+ */
+public class Manager extends Employee
+{
+   private double bonus;
+
+   /**
+    * @param name the employee's name
+    * @param salary the salary
+    * @param year the hire year
+    * @param month the hire month
+    * @param day the hire day
+    */
+   public Manager(String name, double salary, int year, int month, int day)
+   {
+      //super含义一：调用超类中带有对应参数的构造器
+      super(name, salary, year, month, day);
+      bonus = 0;
+   }
+
+   @Override
+   public double getSalary()
+   {
+      //super含义二：调用中带有对应参数的方法
+      System.out.print("这里是父类的getSalary：");
+      double baseSalary = super.getSalary();
+      return baseSalary + bonus;
+   }
+
+   public void setBonus(double b)
+   {
+      bonus = b;
+   }
+}
